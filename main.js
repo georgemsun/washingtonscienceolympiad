8b53512386f3b092d20bab40191c9fb74f0e0bc5
George Sun <gmsun27@gmail.com>
2015-09-04 22:11:09 -0700

    Updated files

--- main.js
+++ main.js
@@ -3,22 +3,16 @@ var main = function() {
 	$('.main h1').css('padding',$ (window).innerHeight()*0.19 + ' 0 ' + $(window).innerHeight()*0.12 + ' 0');
 }
 
-$(document).ready(main);
-
-// $(window).resize(main);
-
-// var currentPage = "home";
-// if(currentPage == "home") {
-// 	$('.home').css('border-bottom', '2px solid white');
-// }
-
-$(document).ready(function() {
+var nav = function() {
 	$('.nav > li').bind('mouseover', function() {
 		$(this).find('ul').css('visibility', 'visible');
-		// $(this).fadeTo(fadeOut, 0, function(){ $(this).find('ul').css('visibility', 'visible'); }).fadeTo(fadeIn_menu, 1);
 	});
 	$('.nav > li').bind('mouseout', function() {
 		$(this).find('ul').css('visibility', 'hidden');
-		// $(this).fadeTo(fadeOut, 0, function(){ $(this).find('ul').css('visibility', 'hidden'); }).fadeTo(fadeIn_menu, 1);
 	});				
-});
+}
+
+$(document).ready(main);
+$(document).ready(nav);
+
+// $(window).resize(main);
