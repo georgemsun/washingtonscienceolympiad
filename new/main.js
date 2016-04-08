38388a9d30cea12bf43e41d25ea93e006001f5be
George Sun <bernard@scioly.org>
2016-04-08 05:52:27 -0700

    Updated files

--- new/main.js
+++ new/main.js
@@ -1,7 +1,7 @@
 var html = function() {
 	$('#javascript').remove();
 	$('#html').html('<link href="https://fonts.googleapis.com/css?family=Roboto:400,100,300,500, 700, 900" rel="stylesheet" type="text/css">');
-	$('#nav').html('<div class="nav"><a href="index.html"><img src="logo.png" width="100px"></a><span class="title">Washington Science Olympiad</span><ul><li><a href="#">Menu <i class="material-icons">menu</i></a></li></ul></div><div class="dropdown"><table><tr><td>About</td><td>Tournaments</td><td>Contribute</td><td>State Board</td></tr><td><a href="#">About Science Olympiad</a></br><a href="#">Meetings and Workshops</a></td><td><a href="#">Register</a></br><a href="#">List of Tournaments</a></br><a href="#">Tournament Schedule</a></br><a href="#">Results History</a></td><td><a href="#">Volunteer</a></br><a href="#">Coach</a></br><a href="#">Donate</a></td><td><a href="#">Board of Directors</a></br><a href="#">Board Nominations</a></br><a href="#">State By-Laws</a></td><tr></tr><tr><td colspan="5"><i class="material-icons">keyboard_arrow_up</i></td></tr></table></div>');
+	$('#nav').html('<div class="nav"><a href="index.html"><img src="logo.png" width="100px"></a><span class="title">Washington Science Olympiad</span><ul><li><a href="#">Menu <i class="material-icons">menu</i></a></li></ul></div><div class="dropdown"><table><tr><td>About</td><td>Tournaments</td><td>Contribute</td><td>State Board</td></tr><td><a href="#">About Science Olympiad</a></br><a href="#">Meetings and Workshops</a></td><td><a href="tournaments.html#1">Register</a></br><a href="tournaments.html#2">List of Tournaments</a></br><a href="tournaments.html#3">Tournament Schedule</a></br><a href="tournaments.html#4">Results History</a></td><td><a href="#">Volunteer</a></br><a href="#">Coach</a></br><a href="#">Donate</a></td><td><a href="#">Board of Directors</a></br><a href="#">Board Nominations</a></br><a href="#">State By-Laws</a></td><tr></tr><tr><td colspan="5"><i class="material-icons">keyboard_arrow_up</i></td></tr></table></div>');
 	$('#footer').html('<div class="footer"><p>Find us on <a href="https://www.facebook.com/washingtonscienceolympiad/" target="blank_">Facebook</a> and join the <a href="https://www.facebook.com/groups/washingtonscienceolympiad/" target="blank_">group</a> for students, coaches, parents, and alumni!</p><p class="copyright">&copy; Washington Science Olympiad 2016 &ndash; All Rights Reserved</p></div>');
 }
 
@@ -44,7 +44,7 @@ var redirect = function() {
 			$('.tabs > .content > div').removeClass('active');
 			$('.tabs > .content > .' + redirect_id).addClass('active');
 			$('.tabs > .content > .' + redirect_id).show().siblings().hide();
-			$('.tabs > ul > li#' + redirect_id).addClass('active').siblings().removeClass('active');	
+			$('.tabs > ul > li#' + redirect_id).addClass('active').siblings().removeClass('active');
 		}
 	}
 }
